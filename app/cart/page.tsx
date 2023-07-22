1e060a8c62267afc523561a9be71d698a1186ed0
crowwan <crowwan@naver.com>
2023-07-22 21:24:21 +0900

    init: next.js 연습용 레포 생성 및 초기 코드 작성

---
+++ app/cart/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Cart() {
+  return <h1>장바구니입니다.</h1>;
+}
+
+export default Cart;
---
+++ app/cart/payment/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Payment() {
+  return <h1>결제페이지입니다.</h1>;
+}
+
+export default Payment;

07c65a25f2e3adf163ab10bb123c8bd555d34931
crowwan <crowwan@naver.com>
2023-07-24 19:39:26 +0900

    feat: 상품 수량 증가 감소 기능 추가

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,16 +1,36 @@
+"use client";
+
+import { useState } from "react";
+
 function List() {
   let products = ["Tomatoes", "Pasta", "Coconut"];
+
   return (
     <div>
       <h4 className="title">상품목록</h4>
       {products.map((product, i) => (
-        <div className="food">
-          <img src={`/food${i}.png`} className="food-img" />
-          <h4>{product} $40</h4>
-        </div>
+        <ListItem name={product} index={i} key={product} />
       ))}
     </div>
   );
 }
 
 export default List;
+
+function ListItem({ name, index }: { name: string; index: number }) {
+  const [count, setCount] = useState(0);
+
+  const onClickBtn = (num: number) => () => {
+    setCount((prev) => prev + num);
+  };
+
+  return (
+    <div className="food">
+      <img src={`/food${index}.png`} className="food-img" />
+      <h4>{name} $40</h4>
+      <span> 상품 수량 : {count}</span>
+      <button onClick={onClickBtn(-1)}> - </button>
+      <button onClick={onClickBtn(1)}> + </button>
+    </div>
+  );
+}

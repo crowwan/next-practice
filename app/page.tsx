9b7feda53e4aa33e1c9cf21955d0bd1fdd8846fd
crowwan <crowwan@naver.com>
2023-07-27 22:39:09 +0900

    feat: 몽고 db 연결 확인 코드 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
+import { connectDB } from "@/util/database";
+
 export default function Home() {
+  (async () => {
+    const client = await connectDB;
+    const db = client.db("board");
+    const result = await db.collection("board").find().toArray();
+    console.log(result);
+  })();
+
   return (
     <div>
       <h4 className="title">애플 후래시</h4>

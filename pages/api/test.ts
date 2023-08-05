424a03e6bb93922d8184b8226ba6a8f48770e0fe
crowwan <crowwan@naver.com>
2023-08-05 14:13:15 +0900

    feat: 게시글 작성 시 list로 리다이랙트 기능 추가, 게시글 삭제 기능 추가

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,4 +1,5 @@
 import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -7,10 +8,22 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     const db = (await connectDB).db("board");
-    const board = db.collection("board");
 
-    board.insertOne(req.body);
-    return res.status(201).json("ok");
+    await db.collection("board").insertOne(req.body);
+    return res.redirect(302, "/list");
+  }
+  if (req.method === "DELETE") {
+    try {
+      const db = (await connectDB).db("board");
+
+      await db
+        .collection("board")
+        .deleteOne({ _id: new ObjectId(req.query.id as string) });
+
+      return res.status(200).json("ok");
+    } catch (err) {
+      return res.status(500).json("error occured");
+    }
   }
   return res.status(200).json("done");
 }

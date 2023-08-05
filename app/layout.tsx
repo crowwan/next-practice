e7a4e5c938bd8f1f1cb74c893ca3c8253d090f9b
crowwan <crowwan@naver.com>
2023-08-05 14:10:46 +0900

    feat: 게시글 작성 페이지 버튼 네비게이션에 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
             Appleforum
           </Link>
           <Link href="/list">List</Link>
+          <Link href="/write">Write</Link>
         </div>
         {children}
       </body>

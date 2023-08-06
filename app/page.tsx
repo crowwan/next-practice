fea7b64539c2c4204fef5aefc5964354d200c85f
crowwan <crowwan@naver.com>
2023-08-06 21:00:19 +0900

    feat: 데이터 베이스 코드 미사용으로 인한 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,4 @@
-import { connectDB } from "@/util/database";
-
 export default function Home() {
-  (async () => {
-    const client = await connectDB;
-    const db = client.db("board");
-    const result = await db.collection("board").find().toArray();
-    console.log(result);
-  })();
-
   return (
     <div>
       <h4 className="title">애플 후래시</h4>

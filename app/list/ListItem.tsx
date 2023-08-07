2fa6957b21ca2cefb90462e90be02430ce06da81
crowwan <crowwan@naver.com>
2023-08-07 20:31:55 +0900

    fix: 게시글 삭제 시 모든 게시글이 화면에서 사라지는 버그 해결

--- app/list/ListItem.tsx
+++ app/list/ListItem.tsx
@@ -5,46 +5,52 @@ import Link from "next/link";
 import React, { useState } from "react";
 
 function ListItem({ result }: { result: Board[] }) {
-  const [animate, setAnimate] = useState("");
   return (
     <>
       {result.map((item) => (
-        <div className={`list-item ${animate}`} key={JSON.stringify(item._id)}>
-          <h4>
-            <Link href={`/detail/${item._id}`}>{item.title}</Link>
-          </h4>
-          <p>{item.content}</p>
-          <Link href={`/edit/${item._id}`}>✏️ edit</Link>
-          <button
-            onClick={(e) => {
-              (async () => {
-                await fetch(`/api/test?id=${item._id}`, {
-                  method: "DELETE",
-                })
-                  .then((res) => {
-                    if (res.status === 200) {
-                      return res.json();
-                    } else {
-                      return Promise.reject("error occurred");
-                    }
-                  })
-                  .then(() => {
-                    setAnimate("hide");
-                    setTimeout(() => {
-                      setAnimate("none");
-                    }, 510);
-                  })
-                  .catch((err) => {
-                    console.error(err);
-                  });
-              })();
-            }}
-          >
-            🗑️ delete
-          </button>
-        </div>
+        <Item item={item} key={item._id} />
       ))}
     </>
+  );
+}
+
+function Item({ item }: { item: Board }) {
+  const [animate, setAnimate] = useState("");
+  return (
+    <div className={`list-item ${animate}`}>
+      <h4>
+        <Link href={`/detail/${item._id}`}>{item.title}</Link>
+      </h4>
+      <p>{item.content}</p>
+      <Link href={`/edit/${item._id}`}>✏️ edit</Link>
+      <button
+        onClick={(e) => {
+          (async () => {
+            await fetch(`/api/test?id=${item._id}`, {
+              method: "DELETE",
+            })
+              .then((res) => {
+                if (res.status === 200) {
+                  return res.json();
+                } else {
+                  return Promise.reject("error occurred");
+                }
+              })
+              .then(() => {
+                setAnimate("hide");
+                setTimeout(() => {
+                  setAnimate("none");
+                }, 510);
+              })
+              .catch((err) => {
+                console.error(err);
+              });
+          })();
+        }}
+      >
+        🗑️ delete
+      </button>
+    </div>
   );
 }
 

b59971af6dd1c9ca73249cb6fbfe85f6da6a883f
crowwan <crowwan@naver.com>
2023-08-07 20:34:38 +0900

    feat: 로그인 아이디가 아니라 이메일로 받게 변경

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -1,27 +1,30 @@
 import { connectDB } from "@/util/database";
 import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcrypt";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    if (req.body.id === "" || req.body.password === "")
+    if (req.body.email === "" || req.body.password === "")
       return res.status(500).json("empty request body");
     try {
       const db = (await connectDB).db("board");
-      console.log(req.body);
-      const users = await db.collection("user");
+
+      const users = db.collection("user");
       const user = await users.findOne({
-        id: req.body.id,
-        password: req.body.password,
+        email: req.body.email,
       });
 
       if (user) return res.status(401).json("user already exists");
 
-      users.insertOne({ id: req.body.id, password: req.body.password });
+      users.insertOne({
+        email: req.body.email,
+        password: await bcrypt.hash(req.body.password, 10),
+      });
 
-      return res.status(201).json("ok");
+      return res.redirect(302, "/");
     } catch {
       return res.status(501).json("database error");
     }

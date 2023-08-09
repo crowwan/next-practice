992fdcb97fe5ed0037257eb9807eebac110c84a0
crowwan <crowwan@naver.com>
2023-08-09 21:09:44 +0900

    feat: 댓글 컴포넌트 추가

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 import React from "react";
+import Comment from "./Comment";
 
 async function Detail({ params }: { params: { id: string } }) {
   const db = (await connectDB).db("board");
@@ -13,6 +14,7 @@ async function Detail({ params }: { params: { id: string } }) {
       <h4>상세 페이지</h4>
       <h4>{board?.title}</h4>
       <p>{board?.content}</p>
+      <Comment parentId={params.id} />
     </div>
   );
 }

d8a120a85f5ecfa152e1497527f700eff750ecfe
crowwan <crowwan@naver.com>
2023-08-29 10:54:41 +0900

    feat: 좋아요 갯수 받아오는 기능 추가

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -12,11 +12,19 @@ async function Detail({ params }: { params: { id: string } }) {
   const board = await db
     .collection("board")
     .findOne({ _id: new ObjectId(params.id) });
+  const likeCount = await db
+    .collection("like")
+    .countDocuments({ boardId: params.id });
 
   return (
-    <div>
+    <div className="board">
       <h4>상세 페이지</h4>
-      <Header title={board?.title} session={session} boardId={params.id} />
+      <Header
+        title={board?.title}
+        session={session}
+        boardId={params.id}
+        likeCount={likeCount}
+      />
       <p>{board?.content}</p>
       <Comment parentId={params.id} />
     </div>

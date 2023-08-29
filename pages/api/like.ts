5e289b951dc718751ffef3a8281b007fd9d0279c
crowwan <crowwan@naver.com>
2023-08-29 10:53:15 +0900

    feat: 좋아요 기능 추가

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -1,3 +1,4 @@
+import { connectDB } from "@/util/database";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -5,6 +6,13 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "GET") {
-    return res.status(200).json("good");
+    return res.status(200).json("ok");
+  }
+  if (req.method === "POST") {
+    const db = (await connectDB).db("board");
+
+    await db.collection("like").insertOne(JSON.parse(req.body));
+
+    return res.status(201).json("created");
   }
 }
